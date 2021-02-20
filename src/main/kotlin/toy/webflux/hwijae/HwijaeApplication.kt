9e16d08a7259b86364e1a6de209b4ed0f17be6e0
hjkwon <hwijae_kwon@tmax.co.kr>
2021-02-20 23:13:13 +0900

    First commit

---
+++ src/main/kotlin/toy/webflux/hwijae/HwijaeApplication.kt
@@ -0,0 +1,11 @@
+package toy.webflux.hwijae
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HwijaeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HwijaeApplication>(*args)
+}
---
+++ src/test/kotlin/toy/webflux/hwijae/HwijaeApplicationTests.kt
@@ -0,0 +1,13 @@
+package toy.webflux.hwijae
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HwijaeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
